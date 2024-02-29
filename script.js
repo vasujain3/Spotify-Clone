efbea660662cae681f17b94a01a6126f7bc611f5
vasujain3 <vasu.jain.3333@gmail.com>
2024-02-29 22:33:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function play1() {
+    document.getElementById("audio").src = "./songs/Tere Bin Bas Ek Pal 128 Kbps.mp3";
+    document.getElementById("audio").load();
+    document.getElementById("audio").play();
+
+};
+function play2() {
+    document.getElementById("audio").src = "./songs/Pehle Bhi Main Tumse Mila Hu_320(PagalWorld.com.cm).mp3";
+    document.getElementById("audio").load();
+    document.getElementById("audio").play();
+
+
+};
+function play3() {
+    document.getElementById("audio").src = "./songs/Wishes Song By Hasan Raheem Talwiinder.mp3";
+    document.getElementById("audio").load();
+    document.getElementById("audio").play();
+
+};
+function play4() {
+    document.getElementById("audio").src = "./songs/Sadqay-(PagalSongs.Com.IN).mp3";
+    document.getElementById("audio").load();
+    document.getElementById("audio").play();
+
+
+};
+
